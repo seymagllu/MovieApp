981ebdddab0ea7e72bca4b2eaf49242ee8a0e495
seymanur.gullu <nurgulluu@gmail.com>
2021-10-12 21:11:28 +0300

    movieapp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MoviesComponent } from './movies/movies.component';
+import { CategoryComponent } from './category/category.component';
+import { MovieComponent } from './movies/movie/movie.component';
+import { MovieDetailComponent } from './movie-details/movie-detail.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ // component
+    AppComponent,
+    NavbarComponent,
+    MoviesComponent,
+    CategoryComponent,
+    MovieComponent,
+    MovieDetailComponent
   ],
-  imports: [
+  imports: [     //module
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [],  //services
+  bootstrap: [AppComponent]  //starter component
 })
 export class AppModule { }
